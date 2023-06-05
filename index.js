93a2c9a539cab03b2dd4235100d4a198e18ed368
Muhammad Hossain <personal.mdhossain@gmail.com>
2023-06-06 01:53:34 +0600

    differnt routes added like(get one blog, all blog and admin login)

--- index.js
+++ index.js
@@ -1,23 +1,57 @@
 const express = require('express');
+const { allblogs, admin } = require('./database/schema')
+
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('You are in home page')
+
+
+
+
+require('./database/db')
+
+app.get('/', async (req, res) => {
+    const dbblogs = await allblogs.find();
+    res.render('./home/index', { blogs: dbblogs })
 })
 
-app.get('/:id', (req, res) => {
-    res.send('You are in a single blog page')
+
+app.get('/find/:id', async (req, res) => {
+    let findOne = await allblogs.find({ _id: req.params.id })
+    findOne = findOne[0]
+    res.render('./singleBlog/index', { findOne });
 })
 
-app.get('/blog/new', (req, res) => {
-    res.send('You are in new blog post page')
+app.get('/admin/login', (req, res) => {
+    res.render('./admin/index')
+})
+app.post('/admin/login', async (req, res) => {
+    try {
+        let adminData = await admin.find();
+        adminData = adminData[0]
+        const { username, password } = req.body;
+        if (username === adminData.username && password === adminData.password) {
+            res.send('you login successfully admin')
+        } else {
+            res.send('invalid user')
+        }
+    } catch (err) {
+        res.send('invalid user')
+    }
 })
 
-app.get('/blog/update', (req, res) => {
-    res.send('You are in update blog page')
-})
+// app.get('/blog/new', (req, res) => {
+//     res.send('You are in new blog post page')
+// })
+
+// app.get('/blog/update', (req, res) => {
+//     res.send('You are in update blog page')
+// })
 
 
 app.listen(5000, () => {
