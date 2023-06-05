89abd59a0c50904e50420e2a0e9dda024d457dcd
Muhammad Hossain <personal.mdhossain@gmail.com>
2023-06-05 13:27:39 +0600

    static file (all css img and js files) created

---
+++ public/js/home.js
@@ -0,0 +1,17 @@
+const menu = document.querySelector('.navbar-ul');
+const hamburgerIcon = document.querySelector('.fa-bars-staggered');
+
+
+hamburgerIcon.addEventListener('click', () => {
+    checkhamburger(hamburgerIcon);
+})
+
+function checkhamburger(icon) {
+    if (icon.classList.contains('fa-bars-staggered')) {
+        icon.classList.replace('fa-bars-staggered', 'fa-xmark')
+        menu.classList.add('togglemenu');
+    } else {
+        icon.classList.replace('fa-xmark', 'fa-bars-staggered')
+        menu.classList.remove('togglemenu');
+    }
+}
