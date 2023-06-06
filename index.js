dfbb231c3105f65b3e909feab05b0b30d1c11329
Muhammad Hossain <personal.mdhossain@gmail.com>
2023-06-06 23:20:00 +0600

    more admin routes added like update blog and delete

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const express = require('express');
-const { allblogs, admin } = require('./database/schema')
+const { allblogs, admin } = require('./database/schema');
+let islogin = false;
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
-app.use(express.urlencoded({ extended: false }))
+app.use(express.urlencoded({ extended: true }))
 app.use(express.json());
 
 
@@ -33,27 +34,76 @@ app.get('/admin/login', (req, res) => {
 app.post('/admin/login', async (req, res) => {
     try {
         let adminData = await admin.find();
-        adminData = adminData[0]
+        adminData = adminData[0];
         const { username, password } = req.body;
         if (username === adminData.username && password === adminData.password) {
-            res.send('you login successfully admin')
+            islogin = true;
+            let blogs = await allblogs.find();
+            res.render('./adminblog/index', { adminblogs: blogs })
         } else {
             res.send('invalid user')
         }
     } catch (err) {
-        res.send('invalid user')
+        console.log(err)
     }
 })
 
-// app.get('/blog/new', (req, res) => {
-//     res.send('You are in new blog post page')
-// })
+app.get('/admin/delete/:id', async (req, res) => {
+    if (islogin) {
+        const { id } = req.params;
+        await allblogs.findOneAndDelete({ id });
+        res.send('delete successfull')
+    } else {
+        res.render('./admin/index')
+    }
+})
 
-// app.get('/blog/update', (req, res) => {
-//     res.send('You are in update blog page')
-// })
+app.get('/admin/edit/:id', async (req, res) => {
+    if (islogin) {
+        let updateblog = await allblogs.find({ _id: req.params.id });
+        res.render('./editblogadmin/index', { updateblog })
+    }
+    else {
+        res.render('./admin/index')
+    }
+})
+app.post('/update/:id', async (req, res) => {
+    if (islogin) {
+        // const updateblog = await allblogs.findById(req.params.id);
+        const { heading, smallHeading, img, blog } = req.body;
+        console.log(heading)
+        console.log(smallHeading)
+        console.log(img)
+        console.log(blog)
+
+        const filter = { _id: req.params.id };
+        const update = {
+            $set: {
+                img: img,
+                heading: heading,
+                smallblog: smallHeading,
+                bigblog: blog
+            }
+        };
+        const updateblogs = await allblogs.find()
+        const result = await allblogs.findByIdAndUpdate(filter, update)
+        console.log(result)
+        res.render('./adminblog/index', { adminblogs: updateblogs })
+    }
+    else {
+        res.render('./admin/index')
+    }
+
+})
+
+
+
+
+
 
 
 app.listen(5000, () => {
     console.log(`server is running on port ${port}`)
 })
+
+
